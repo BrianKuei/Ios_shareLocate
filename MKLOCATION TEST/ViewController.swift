e1d530b09ba780461dc0aea1d833af26ca45f1a7
Sunbu <vx3012564897@gmail.com>
2019-06-11 17:12:27 +0800

    Initial Commit

---
+++ MKLOCATION TEST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MKLOCATION TEST
+//
+//  Created by Sunbu on 2019/6/11.
+//  Copyright © 2019 Sunbu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
